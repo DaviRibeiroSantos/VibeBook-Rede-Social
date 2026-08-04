47449508c313d1fc95495b346d1d2012de0c2163
Davi Ribeiro Santos Faria <daviribeirao71@gmail.com>
2026-08-03 21:27:05 -0300

    CSS melhorado
    CSS agora com cores e tamanhos de fontes melhorados e botão com cor e animação adicionado

--- frontend/main.js
+++ frontend/main.js
@@ -1,16 +1,16 @@
-const formulario = document.getElementById('meuFormulario');
+const formulario = document.getElementById('cadastroForm');
     const inputEmail = document.getElementById('emailJs');
     const mensagem = document.getElementById('mensagem');
 
     formulario.addEventListener('submit', function(evento) {
-        // Expressão regular simples para validar e-mail
+      
         const padraoEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         
         if (!padraoEmail.test(inputEmail.value)) {
             mensagem.style.color = 'red';
             mensagem.textContent = 'Por favor, insira um e-mail válido.';
             
-            // Impede o envio do formulário se o e-mail for inválido
+        
             evento.preventDefault(); 
         } else {
             mensagem.style.color = 'green';
